65bc6830263dd0969c8f1d0ae0adf77d2e688ebd
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-07-06 20:33:28 -0700

    added function properties, timer works

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-
+// Questions 
+const questions = [
+    {
+      question: "What is 2 + 2?",
+      answer: [
+        {
+          text: "4",
+          isCorrect: true
+        },
+        {
+          text: "5",
+          isCorrect: false
+        },
+        {
+          text: "6",
+          isCorrect: false
+        },
+        {
+          text: "7",
+          isCorrect: false
+        }
+      ]
+    }
+  ]
   let currentQuestionIndex = 0;
   let score = 0;
   let timeLeft = 60;
@@ -9,11 +32,13 @@
   var startContainer = document.getElementById("start-container");
   var quizContainer = document.getElementById("quiz-container");
   var endContainer = document.getElementById("end-container");
+  
 
   // Buttons
   var startBtn = document.getElementById("start-btn");
   var nextBtn = document.getElementById("next-btn");
   var submitBtn = document.getElementById("submit-btn");
+  var answerBtn = document.querySelectorAll(".answer-btn");
 
   //Number Elements
   var timerEl = document.getElementById("timer");
@@ -38,27 +63,72 @@
   
   // Submit button click event
   submitBtn.addEventListener("click", submitScore);
-  
-  function startQuiz() {
 
+  //start of the quiz
+  function startQuiz() {
+    startContainer.style.display = "none";
+    quizContainer.style.display = "block";
+    startBtn.disabled = true;
+    nextBtn.disabled = false;
+    startTimer();
+    showNextQuestion();
   }
   
   function showNextQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionsLeftEl.innerHTML = questionNo + "." + currentQuestion.question;
 
+    currentQuestion.answers.forEach(answer => {
+      let button = document.createElement("button");
+      button.innerText = answer.text;
+      button.classList.add("btn");
+      answerBtn.appendChild(button);
+    });
+}
+
+function resetState() {
+    nextBtn.style.display = "none";
+    while (answerBtn.firstChild) {
+      answerBtn.removeChild(answerBtn.firstChild);
+    }
   }
-  
   function checkAnswer() {
-
+    let currentQuestion = questions[currentQuestionIndex];
+    let answer = currentQuestion.answers[0];
+    if (answer.isCorrect) {
+      score++;
+    }
   }
   
   function endQuiz() {
-
+    clearInterval(timerInterval);
+    quizContainer.style.display = "none";
+    endContainer.style.display = "block";
+    finalScoreElement.textContent = score;
+  
+    if (score > highestScore) {
+      highestScore = score;
+      highScoreElement.textContent = `${highestScore} - ${highestScorer}`;
+    }
+  
+    highScoreContainer.style.display = "block";
   }
+
   
   function submitScore() {
 
   }
   
   function startTimer() {
-
+    timerInterval = setInterval(() => {
+      timeLeft--;
+      timerEl.textContent = timeLeft;
+  
+      if (timeLeft === 0) {
+        clearInterval(timerInterval);
+        endQuiz();
+      }
+    }, 1000);
   }
