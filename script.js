53daf57cc6c18c54da60bdaf809dac59c3e21905
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-07-06 20:08:43 -0700

    updated CSS and HTML, working on script

--- script.js
+++ script.js
@@ -5,18 +5,22 @@
   let highestScore = 10;
   let highestScorer = "Anthony";
   
-  // Elements
+  // Containers 
   var startContainer = document.getElementById("start-container");
   var quizContainer = document.getElementById("quiz-container");
   var endContainer = document.getElementById("end-container");
+
+  // Buttons
   var startBtn = document.getElementById("start-btn");
   var nextBtn = document.getElementById("next-btn");
-  var timerElement = document.getElementById("timer");
-  var scoreElement = document.getElementById("score");
-  var questionsLeftElement = document.getElementById("questions-left");
-  var finalScoreElement = document.getElementById("final-score");
-  var nameInput = document.getElementById("name-input");
   var submitBtn = document.getElementById("submit-btn");
+
+  //Number Elements
+  var timerEl = document.getElementById("timer");
+  var scoreEl = document.getElementById("score");
+  var questionsLeftEl = document.getElementById("questions-left");
+  var finalScoreEl = document.getElementById("final-score");
+  var nameInput = document.getElementById("name-input");
   
   // Start Button
   startBtn.addEventListener("click", startQuiz);
